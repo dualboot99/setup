776d0a61e0caa4bb8b1d483ac48c8f5824430984
dualboot99 <rodrigomdmourato@gmail.com>
2025-01-12 13:18:19 +0000

    Reorder windows scripts

---
+++ linux/initial_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt install && sudo apt upgrade && sudo apt autoremove
+sudo apt-get update && sudo apt-get upgrade
+sudo apt install unzip
+curl -s https://ohmyposh.dev/install.sh | bash -s
+git clone https://github.com/JanDeDobbeleer/oh-my-posh.git
+cp .profile ~/.profile
